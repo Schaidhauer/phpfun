911e4b47265a7f90f4c00d7a5c13c6b9725f5543
Rianne Schaidhauer <rianne.schaidhauer@gmail.com>
2018-04-23 17:13:34 -0300

    Funcao para timeAGO

--- app/core/class.help.php
+++ app/core/class.help.php
@@ -171,6 +171,40 @@ Class Help{
 		}
 	}
 	
+	
+	function time_ago($date) {
+		if (empty($date)) {
+			return "No date provided";
+		}
+		$periods = array("segundo", "minuto", "hora", "dia", "semana", "mes", "ano", "decada");
+		$lengths = array("60", "60", "24", "7", "4.35", "12", "10");
+		$now = time();
+		$unix_date = strtotime($date);
+		// check validity of date
+		if (empty($unix_date)) {
+			return "Bad date";
+		}
+		// is it future date or past date
+		if ($now > $unix_date) {
+			$difference = $now - $unix_date;
+			$tense = "atrás";
+		} else {
+			$difference = $unix_date - $now;
+			$tense = "agora";
+		}
+		for ($j = 0; $difference >= $lengths[$j] && $j < count($lengths) - 1; $j++) {
+			$difference /= $lengths[$j];
+		}
+		$difference = round($difference);
+		if ($difference != 1) {
+			if ($periods[$j] == 'mes')
+				$periods[$j].= "es";
+			else
+				$periods[$j].= "s";
+		}
+		return "$difference $periods[$j] {$tense}";
+	}
+	
 }
 
 ?>
