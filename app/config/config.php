9d4b3dcdcd6cf15a8cb62b83032be96ade1270c8
Rianne Schaidhauer <rianne.schaidhauer@gmail.com>
2018-06-14 11:14:52 -0300

    novas variaveis de config

--- app/config/config.php
+++ app/config/config.php
@@ -7,7 +7,7 @@
 | utilizado normalmente para aspaginas Home do site, se nao tiver nada do lado o /, cai nessa classe.
 |
 */
-$config_g['defaultClass'] = 'hello';
+$config_g['defaultClass'] = 'dashboard';
 
 
 /*
@@ -19,7 +19,7 @@ $config_g['defaultClass'] = 'hello';
 | Para deixar no padrão é necessário deixar em /
 |
 */
-$config_g['systemFolder'] = '/yourfolder';
+$config_g['systemFolder'] = '/phpfun';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +33,18 @@ $config_g['protocolo']   = 'http';
 
 /*
 |--------------------------------------------------------------------------
+| AUDIT
+|--------------------------------------------------------------------------
+|
+| Auditar objetos do BD, com alterações e inserções
+|
+| true ou false
+|
+*/
+$config_g['audit']   = true;
+
+/*
+|--------------------------------------------------------------------------
 | PERMISSOES
 |--------------------------------------------------------------------------
 |
@@ -42,7 +54,29 @@ $config_g['protocolo']   = 'http';
 | true ou false
 |
 */
-$config_g['allowAllClasses']   = true;
+$config_g['allowAllClasses']   = false;
+
+/*
+|--------------------------------------------------------------------------
+| PAGINACAO
+|--------------------------------------------------------------------------
+|
+| Define quantas linhas vao ser apresentadas antes de começar a paginar
+|
+|
+*/
+$config_g['paginarMax'] = 10;
+
+/*
+|--------------------------------------------------------------------------
+| SESSION TIMEOUT
+|--------------------------------------------------------------------------
+|
+| Tempo para expirar a sessao do browser
+|
+|
+*/
+$config_g['session_timeout'] = 9600;
 
 /*
 |--------------------------------------------------------------------------
@@ -52,18 +86,20 @@ $config_g['allowAllClasses']   = true;
 | ldap ou bd
 |
 */
-$config_g['login_tipo']        = 'ldap';
+$config_g['login_tipo']          = 'ldap';
 
 /*LOGIN via LDAP*/
-$config_g['login_ldap_host']   = '';
-$config_g['login_ldap_port']   = '';
-$config_g['login_ldap_domain'] = '';
-$config_g['login_ldap_dn']     = '';
-$config_g['login_ldap_group']  = '';
+$config_g['login_ldap_host']     = '';
+$config_g['login_ldap_port']     = '';
+$config_g['login_ldap_domain']   = '';
+$config_g['login_ldap_dn']       = '';
+$config_g['login_ldap_group']    = '';
 
-/*LOGIN via BD*/
-$config_g['login_bd_table']    = '';
-$config_g['login_bd_usuario']  = '';
-$config_g['login_bd_senha']    = '';
+/*LOGIN via BD ou salvar infos do LDAP para aplicar permissoes*/
+$config_g['login_bd_table']      = 'lf_usuarios';
+$config_g['login_bd_permissoes'] = 'lf_usuarios_permissoes';
+$config_g['login_bd_rel_grupo']  = 'lf_usuarios_grupos';
+$config_g['login_bd_usuario']    = 'usuario';
+$config_g['login_bd_senha']      = 'senha';
 
 ?>
