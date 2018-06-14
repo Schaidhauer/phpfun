83d689d58b0982f086bb44b96f1752c89bc3c2f4
Rianne Schaidhauer <rianne.schaidhauer@gmail.com>
2018-06-14 11:16:35 -0300

    login controller

--- app/controls/control.login.php
+++ app/controls/control.login.php
@@ -1,17 +1,14 @@
 <?php
-require_once("app/core/class.core.php");
 require_once("app/core/class.crud.php");
 
 Class LoginController{
 
-	public $core;
 	public $crud;
 	public $objVar;
 	public $formconfig;
 	
-	public function __construct(){
-		$this->core = new Core();
-		
+	public function __construct()
+	{
 		$this->crud = new CrudBootstrap();
 	}
 	
